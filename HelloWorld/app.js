505151c838e2d8429e435bd9c16b88d462c9abd0
James Kim <jameskim@Jamess-MacBook-Pro.local>
2016-01-31 21:45:59 -0800

    updated html w/ lil content

--- HelloWorld/app.js
+++ HelloWorld/app.js
@@ -1,3 +1,18 @@
+/*
+
+    What is going on in the code??
+
+    In this code, we will be going over angular.module, controllers, and $scope.
+
+        angular.module is a colleciton of angular services, directives, controllers, filters, and configuration information.
+        angular.module takes two arguments, the module name (in which case we used "app") and an array of dependencies.
+        (for this application we do not require any depdencies, but still require to put in an empty array; if not you will get Uncaught Errors.)
+
+        ng-controller is an angular directive which acts on the whole MVC components.
+
+ */
+
+
 var app = angular.module("app", []);
 app.controller("mainCtrl", function($scope){
     $scope.message = "Hello World!";
